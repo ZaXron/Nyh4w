85824986b36fbd3a2ec34a62e7faeb7e92b7cf87
ZaXron <76252428+ZaXron@users.noreply.github.com>
2024-10-06 17:56:28 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,40 @@
+//#include <iostream>
+//using namespace std;
+//int main() {
+//    
+// int size;
+// cout << "enter your size ->";
+// cin >> size;
+//
+// int* arr = new int[size];
+//    
+// cout << "enter your " << size << " elements ->";
+// for (int i = 0; i < size; i++)
+//    {
+//        cin >> arr[i];
+//    }
+//
+// cout << "first array ->";
+// for (int i = 0; i < size; i++)
+//    {
+//      cout << arr[i] << " ";
+//    }
+// cout << endl;
+//
+// int* arr2 = new int[size + 1];
+// for (int i = 0; i < size; i++)
+//    {
+//        arr2[i] = arr[i];
+//    }
+// arr2[size] = 100;
+//
+//
+// cout << "second array -> ";
+// for (int i = 0; i < size + 1; i++)
+//    {
+//        cout << arr2[i] << " ";
+//     }
+// cout << endl;
+// delete[] arr;
+// delete[] arr2;
+//}
